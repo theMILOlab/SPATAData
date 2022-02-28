77c44aeccfdc6a7ca8f26917eb8b3c880b9b215c
heilandd <dieter.henrik.heiland@uniklinik-freiburg.de>
2022-02-28 15:33:40 +0100

    New Package

---
+++ R/Functions.R
@@ -0,0 +1,117 @@
+#' @title  getData
+#' @author Dieter Henrik Heiland
+#' @description getData
+#' @inherit 
+#' @return 
+#' @examples 
+#' 
+#' @export
+#' 
+
+getData <- function(sample.name, folder=NULL){
+  
+  org.wd <- getwd()
+  
+  if(is.null(folder)){
+    folder=paste0(getwd(), "/Data.download")
+    if(!file.exists(folder)){ 
+      dir.create("Data.download"); 
+      }else{
+        "Download Folder '.../Data.download...' exists, data will be saved in the default location"
+    }
+  }else{
+    if(file.exists(folder)){ 
+      setwd(folder)}else{"Folder does not exist"}}
+  
+  setwd(folder)
+  message(paste0(Sys.time()," ---- Downloads will be in the: ", folder, " folder ---- "))
+  
+
+# Check if sample name is part of the listed data ------------------------
+
+  
+  data <- SPATAData::list.data() %>% filter(sample %in% sample.name)
+  
+  if(nrow(data)!=0){
+    
+    out <- purrr::map_df(.x=1:nrow(data), .f=function(i){
+      
+      #Set parameters
+      link <- data[i, "link"]
+      name <- data[i, "sample"]
+      localisation <- paste0(folder,"/",name)
+      
+      utils::download.file(link, paste0(name, ".RDS") )
+      
+      out <- data.frame(sample.name=name, localisation=localisation)
+      return(out)
+      
+    })
+    
+  }else{" No match between sample.name and the listed data were found. Please use a valid sample.name/s"}
+ 
+  setwd(org.wd)
+  return(list(folder, out)) 
+}
+
+
+
+#' @title  list.data
+#' @author Dieter Henrik Heiland
+#' @description list.data
+#' @inherit 
+#' @return 
+#' @examples 
+#' 
+#' @export
+#' 
+loadData <- function(sample.name, folder){
+  
+  file <- paste0(folder, "/", sample.name, ".RDS")
+  
+  if(file.exists(file)){readRDS(file)}else{"Sample is not in the download folder"}
+  
+}
+
+
+
+#' @title  list.data
+#' @author Dieter Henrik Heiland
+#' @description list.data
+#' @inherit 
+#' @return 
+#' @examples 
+#' 
+#' @export
+#' 
+list.data <- function(){read.csv(system.file("data", "source.csv", package = "SPATAData"), sep=";")}
+
+
+#' @title  CleanObject
+#' @author Dieter Henrik Heiland
+#' @description CleanObject
+#' @inherit 
+#' @return 
+#' @examples 
+#' 
+#' @export
+#'
+
+CleanObject <- function(object){
+  
+  message(paste0(Sys.time()," ---- Remove all slots with processed data ---- "))
+  
+  sample <- SPATA2::getSampleNames(object)
+  object@cnv[[sample]] <- NULL
+  object@spatial[[sample]] <- NULL
+  object@fdata[[sample]] <- object@fdata[[sample]] %>% dplyr::select(barcodes,sample,segmentation,nCount_Spatial,nFeature_Spatial)
+  
+  return(object)
+}
+
+
+
+
+
+
+
